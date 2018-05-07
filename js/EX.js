5ee9efd73b1168c3dcc762248bdc105b2c547e5f
R1M19E19 <Garcia.209134611.2c@gmail.com>
2018-05-07 11:08:08 -0500

    yaestacreo

---
+++ js/EX.js
@@ -0,0 +1,9 @@
+function toggler1(){
+    document.getElementById("res1").classList.toggle("invisible");
+}
+function toggler2(){
+    document.getElementById("res2").classList.toggle("invisible");
+}
+function toggler3(){
+    document.getElementById("res3").classList.toggle("invisible");
+}
